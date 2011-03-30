edd55f4eb3f120266c12eaab83d25e1422ee67b1
twtrinh <tommy.trinh@gmail.com>
2011-03-29 23:04:49 -0400

    Added user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name											"Tommy Chang"
+	user.email										"tommy.chang@email.com"
+	user.password									"foobar"
+	user.password_confirmation		"foobar"
+end
